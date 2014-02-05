f0bf1f87e1cb988547e29e79f25617d083288fd8
Aidan Sawyer <aks5238@rit.edu>
2014-02-05 00:53:53 -0500

    commented and cleaned up pal.py

--- Palindrome/palindrome.py
+++ Palindrome/palindrome.py
@@ -4,20 +4,16 @@
 #language: python3
 """
 
-#import sys to enable access to stdin
+#enable access to stdin
 import sys 
 
 def main():
 	"""loop through stdin, call is_palindrome() on each line. 
 	if it is a palindrome, prints it out. """
 
-	#print( is_palindrome("dad") )
-	#print( is_palindrome("daddy") )
-	#print( is_palindrome("Dad") )
-	#print( is_palindrome("madam, i'm Adam") )
-
 	for line in sys.stdin:
 		if is_palindrome(line):
+			#print that line without its newline character
 			print( line.strip("\n") )
 		else:
 			pass
@@ -28,13 +24,19 @@ def is_palindrome(l):
 	fd = 0           #start at 0th index
 	bk = len(l)-1    #last index of line
 
-	while(fd<=bk): #loop through until back and forward indexes cross
+	#loop until back and forward indexes cross
+	while(fd<=bk): 
+
+		#if current couple matches, change indexes to next inner pair
 		if(l[fd] == l[bk]):
 			fd = advance(fd)
 			bk = retreat(bk)
+
+		#if current couple does not match up, it is not a palindrome
 		else: 
 			return False
-		
+	
+	#this only gets hit if all have been matches		
 	return True
 
 def advance(i):
@@ -58,6 +60,8 @@ def cleanLine(l):
 
 	#make lowercase
 	line = line.lower()
+
+	#remove new line character
 	line = "".join(line.strip("\n"))
 
 	#return
