d97a3677ef51f917b07a177f8b0b5a3adb08793d
Aidan Sawyer <aks5238@rit.edu>
2014-02-19 02:12:44 -0500

    RH - refactor of step4

--- RubyHistogram/histogram.rb
+++ RubyHistogram/histogram.rb
@@ -2,20 +2,26 @@
 #language: ruby
 #activity:ruby histogram
 
+
+bag = Hash.new(0)
+
 #for each line in standard input
 $stdin.each{ |line| 
 
     #chomp off any terminating characters (\n)
-    lsWords = line.chomp().split(/ +/)
+    line = line.chomp()
+
+    #remove all non-alphabetic or non-space characters
+    line = line.gsub(/\p{^Alpha}/,"")
+
+    #remove any leading spaces
+    line = line.sub(/ +/,"")
+
+    #split line into a list of words
+    lsWords = line.split(/ +/)
 
     #for each individual 'word' in line
     lsWords.each{ |word|
-
-        #remove all non-alphabetic or non-space characters
-        word = word.gsub(/\p{^Alpha}/,"")
-
-        #remove any leading spaces
-        word = word.sub(/ +/,"")
 
         #print word
         puts(word)
