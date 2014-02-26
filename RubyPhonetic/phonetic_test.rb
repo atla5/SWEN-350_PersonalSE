2ebce95c4c7ffee61a60d06909976c31c703b00c
Aidan Sawyer <aks5238@rit.edu>
2014-02-26 11:50:53 -0500

    RP-ran out of time in class writing first tests
    NOTES: need to refacteor form_phonetic(str) to take away need for 'A2P'

--- RubyPhonetic/phonetic_test.rb
+++ RubyPhonetic/phonetic_test.rb
@@ -3,6 +3,21 @@ require 'test/unit'
 
 class PhoneticTest < Test::Unit::TestCase
 
+  #test with incorrect input
+  def test_bad_inputs
+    
+    #non-string input
+    assert_raise ArgumentError do
+      Phonetic.translate(123)
+    end 
+
+    #string input without A2P
+    assert_raise ArgumentError do
+      Phonetic.translate('RIT')
+    end
+  end
+
+  #test P2A 
   def test_rit_to_phonetic
     assert_equal 'ROMEO INDIA TANGO', Phonetic.to_phonetic('RIT')
   end
