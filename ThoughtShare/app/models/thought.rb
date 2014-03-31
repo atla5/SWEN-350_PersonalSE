452f45ffc0349b0bc33002531cceb808e49db990
Aidan Sawyer <aks5238@rit.edu>
2014-03-31 09:57:53 -0400

    TSL1-added custom messages to validations

--- ThoughtShare/app/models/thought.rb
+++ ThoughtShare/app/models/thought.rb
@@ -4,9 +4,11 @@ class Thought < ActiveRecord::Base
 
   #set character limits to 5 <= numChar <= 154
   validates :thought, length: { minimum: 5, maximum: 154 }
+	message: "thought must be between 5 and 154 characters long"
 
   #ensure no duplicate thoughts
   validates :thought, uniqueness: true
+	message: "thoughts must be unique"
 
   #set multiplicities  
   belongs_to :thinker #many-to-one thinker->thought.
