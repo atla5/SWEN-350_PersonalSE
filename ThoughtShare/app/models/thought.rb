ac8a9045fbd9ae0b8b6d8e1150358d1f945461a5
Aidan Sawyer <aks5238@rit.edu>
2014-03-31 10:02:49 -0400

    TSL1-syntax errors for messages. will undo

--- ThoughtShare/app/models/thought.rb
+++ ThoughtShare/app/models/thought.rb
@@ -3,12 +3,12 @@ class Thought < ActiveRecord::Base
   ## - validations - ##
 
   #set character limits to 5 <= numChar <= 154
-  validates :thought, length: { minimum: 5, maximum: 154 }
-	message: "thought must be between 5 and 154 characters long"
+  validates :thought, length: { minimum: 5, maximum: 154 , \
+	message: "thought must be between 5 and 154 characters long"}
 
   #ensure no duplicate thoughts
-  validates :thought, uniqueness: true
-	message: "thoughts must be unique"
+  validates :thought, uniqueness: {true \
+	message: "thoughts must be unique"}
 
   #set multiplicities  
   belongs_to :thinker #many-to-one thinker->thought.
