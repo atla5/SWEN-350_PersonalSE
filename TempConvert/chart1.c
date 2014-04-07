982d313e7becfc81923a5728f42fcd4b8f7b76ef
Aidan Sawyer <aks5238@rit.edu>
2014-04-07 01:47:56 -0400

    TC-chart1 complete

--- TempConvert/chart1.c
+++ TempConvert/chart1.c
@@ -3,14 +3,22 @@
 
 int convert(int faren){
     /*convert a farenheit to celsius and return */
-    return (5/9.0)(faren-32);
+    return (5*(faren-32))/9;
 }
 
 int main(){
-  
-    int i;
-    for(i=0;i<=300;i+20){
-        printf("    %d     %d\n",i,convert(i));
+ 
+    printf("Farenheit-Celsium\n");
+ 
+    int f;
+    for(f=0;f<=300;f+=20){
+      if(f<10){
+        printf("    %d        %d\n",f,convert(f));
+      }else if(f<100){
+        printf("    %d       %d\n",f,convert(f));
+      }else{
+        printf("    %d      %d\n",f,convert(f));
+      }
     }
     return 1;
 }
